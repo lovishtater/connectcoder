cc71a95fe3f581ed69b9d4e6ab83bb2c43b5aeb2
lovishtater08 <lovish.t@source.one>
2023-03-07 18:44:49 +0530

    ios installation changes

--- src/screens/Feed.tsx
+++ src/screens/Feed.tsx
@@ -2,12 +2,17 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, {useState} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import {Button} from 'react-native-paper';
+import Appbar from 'react-native-paper/lib/typescript/components/Appbar/Appbar';
+import { COLORS } from '../constants';
 
 const FeedScreen = () => {
   const [counter, setCounter] = useState(0);
 
   return (
     <View style={styles.container}>
+      {/* <Appbar style={{backgroundColor: COLORS.primary}}>
+        <Appbar.Content title="Feed" />
+      </Appbar> */}
       <Text style={{color: 'white', fontSize: 30}}>{counter}</Text>
     </View>
   );
@@ -16,7 +21,7 @@ const FeedScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#dc3',
+    backgroundColor: COLORS.background,
     alignItems: 'center',
     justifyContent: 'center',
   },
